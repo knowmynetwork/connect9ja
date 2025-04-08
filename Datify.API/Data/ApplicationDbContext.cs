3d0d66446ad5605d8234dfe7b56403723eda8f48
ololade oyebanji <loladeking@gmail.com>
2025-04-08 20:22:46 +0100

    more

--- Datify.API/Data/ApplicationDbContext.cs
+++ Datify.API/Data/ApplicationDbContext.cs
@@ -9,4 +9,19 @@ public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<
     public DbSet<Document> Documents { get; set; }
     public DbSet<FlutterWaveVerificationResponse> FlutterWaveVerificationResponses { get; set; }
     public DbSet<OtpVerification> OtpVerifications { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ApplicationUser>(entity =>
+        {
+            
+            entity.Property(e => e.DateOfBirth)
+                .HasColumnType("timestamp with time zone")
+                .HasConversion(
+                    v => v.Value.ToUniversalTime(), // Convert to UTC before saving
+                    v => DateTime.SpecifyKind(v, DateTimeKind.Utc)); // Convert to UTC when reading;
+
+        });
+        base.OnModelCreating(modelBuilder);
+    }
 }
